d6a81bbf33264c58d991fecd7a7d465886715fc6
Kazi Mohammad Foysal <foysalkazimd01@gmail.com>
2024-03-11 14:17:00 +0600

    succed to load data from db and use them to update settings

--- inc/register-texonomy.inc.php
+++ inc/register-texonomy.inc.php
@@ -23,28 +23,27 @@ function getData(){
                 }
 
             }
+            $tax_id = (isset($tax_id) && !empty($tax_id)) ? $tax_id : '';
+            $tax_name = (isset($tax_name) && !empty($tax_name)) ? $tax_name : '';
+            $post_types = (isset($post_types) && !empty($post_types)) ? $post_types : [];
+            $hirarchial = (isset($hirarchial) && $hirarchial == 'on') ? true : false;
+            $query_var = (isset($query_var) && $query_var == 'on') ? true : false;
+            $show_admin_column = (isset($show_admin_column) && $show_admin_column == 'on') ? true : false;
+            
             function kmfdtr_texonomy_temp($tax_name = '', $tax_id = '', $hirarchial = false, $query_var = false, $show_admin_column = false, $post_types = []){
                 register_taxonomy($tax_id, $post_types, [
-                    'hierarchical' => (isset($hirarchial) && $hirarchial == 'on') ? true : false,
+                    'hierarchical' => $hirarchial,
                     'label' => $tax_name,
-                    'query_var' => (isset($query_var) && $query_var == 'on') ? true : false,
-                    'show_admin_column' => (isset($show_admin_column) && $show_admin_column == 'on') ? true : false,
+                    'query_var' => $query_var,
+                    'show_admin_column' => $show_admin_column,
                 ]);
             }
-            if(isset($tax_name) && isset($tax_id) && isset($hirarchial) && isset($query_var) && isset($show_admin_column) && isset($post_types)){
+            if(function_exists('kmfdtr_texonomy_temp')){
                 kmfdtr_texonomy_temp($tax_name, $tax_id, $hirarchial, $query_var, $show_admin_column, $post_types);
-        }else{
-            // get the error
-            echo var_dump($hirarchial);
-            die();
-        }
-        // if(function_exists('kmfdtr_texonomy_temp')){
-        //     add_action('init', 'kmfdtr_texonomy_temp');
-        // }
+            }
     }
 }
 }
-// add_action('save_post', 'getData');
 if(function_exists('getData')){
     add_action('init', 'getData');
 }
--- texonomy.php
+++ texonomy.php
@@ -19,11 +19,11 @@ function admin_enqueue_scripts_callback(){
     //Add the Select2 CSS file
     wp_enqueue_style( 'select2-css', 'https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css', array(), '4.1.0-rc.0');
 
-    //Add the Select2 JavaScript file
-    wp_enqueue_script( 'select2-js', 'https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js', 'jquery', '4.1.0-rc.0');
+    //Add the Select2 JavaScript file in footer
+    wp_enqueue_script( 'select2-js', 'https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js', 'jquery', '4.1.0-rc.0', true );
 
     //Add a JavaScript file to initialize the Select2 elements
-    wp_enqueue_script( 'select2-init',plugin_dir_url( __FILE__ ).'assets/select2-init.js' , 'jquery', '4.1.0-rc.0');
+    wp_enqueue_script( 'select2-init', plugin_dir_url( __FILE__ ) . 'js/select2-init.js', array('jquery'), '1.0.0', true );
 
 }
 add_action( 'admin_enqueue_scripts', 'admin_enqueue_scripts_callback' );
